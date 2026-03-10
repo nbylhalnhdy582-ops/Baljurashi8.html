b8d97c3efe5a9d9fa6209af8f6a8f98819d9704b
nbylhalnhdy582-ops <nbylhalnhdy582@gmail.com>
2026-03-10 03:21:40 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+// زر الترحيب
+const welcomeScreen = document.getElementById('welcomeScreen');
+const startBtn = document.getElementById('startBtn');
+const mainContent = document.getElementById('mainContent');
+
+startBtn.addEventListener('click', () => {
+  welcomeScreen.style.display = 'none';
+  mainContent.style.display = 'block';
+  alert('مرحباً بك في بلجرشي 🇸🇦'); // رسالة ترحيب تفاعلية
+});
+
+// همبرقر
+function toggleMenu(){
+  const menu = document.getElementById('menu');
+  if(menu.style.display === "block"){
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
